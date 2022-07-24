49e4355cc900222fd7e02a7f5eabb77d110f40a9
Jiraporndt <108787068+Jiraporndt@users.noreply.github.com>
2022-07-25 00:24:06 +0700

    Update Homework02.py

--- Homework02.py
+++ Homework02.py
@@ -1,20 +1,36 @@
 p = int(input("How many sticks (N) in the pile? : "))
-print("There are " , p, " sticks in the pile.")
+print("There are", p, "sticks in the pile")
 name = str(input("What is your name? : "))
 
 i = 0
-while p != 0:
-  n = int(input(name + " How many sticks will you take :"))
-  if n > 2:
-    print("No you cannot take more than 2 sticks!")
-  elif n < 1:
-    print("No you cannot take less than 1 sticks!")
-  elif n > p:
-    print("There are no enough sticks to take.")
-  else :
-    i += 1
-    p = p-n
-    if p == 0:
-      print("OK. There is no sticks left in the pile. You spent", i, "times.")
+import random
+while (p != 0):
+    if (i%2 == 0):
+        n = int(input(name + ", how many sticks you will take : "))
+        if (n > 2):
+            print("No you cannot take more than 2 sticks!\n")
+        elif (n < 1):
+            print("No you cannot take less than 1 stick!\n")
+        elif (n > p):
+            print("There are no sticks enough to take.\n")
+        else :
+            i += 1
+            p = p-n
+            if (p == 0):
+                print(name, ", takes the last stick.")
+                print("Computer win !!!!")
+            else :
+                print("There are", p, "sticks in the pile.\n")
     else :
-      print("There are" , p, "sticks in the pile")
+        com = random.randint(1,2)
+        print("computer, take : ", com)
+        if (p - com < 0):
+            print("There are no enough sticks to take.")
+        else :
+            p = p-com
+            i += 1
+            if (p == 0):
+                print("Computer, take the last stick.")
+                print(name, ", Win (Computer, am sad T_T)")
+            else :
+                print("There are", p, "sticks in the pile.\n")
