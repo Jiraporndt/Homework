72a0f2a25eec912365e9ed430520e01f4acfe792
Jiraporndt <108787068+Jiraporndt@users.noreply.github.com>
2022-08-09 23:56:07 +0700

    Add files via upload

---
+++ Homework05.py
@@ -0,0 +1,17 @@
+import random
+z=[]
+a=(input("Number: "))
+z.append(a)
+b=(input("Number: "))
+z.append(b)
+c=(input("Number: "))
+z.append(c)
+d=(input("Number: "))
+z.append(d)
+
+x=[]
+for i in range(10000):
+    random.shuffle(z)
+    e=int(z[0]+z[1]+z[2]+z[3])
+    x.append(e)
+print("Max Number is",format(max(x)))
