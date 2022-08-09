b574b4157753f4c8bb8d8136677df0ff5db956ea
Jiraporndt <108787068+Jiraporndt@users.noreply.github.com>
2022-08-10 00:21:46 +0700

    Update Homework05.py

--- Homework05.py
+++ Homework05.py
@@ -1,16 +1,10 @@
 import random
 z=[]
-a=(input("Number: "))
-z.append(a)
-b=(input("Number: "))
-z.append(b)
-c=(input("Number: "))
-z.append(c)
-d=(input("Number: "))
-z.append(d)
+for i in range (0,4):
+    z.append(input("Number: "))
 
 x=[]
-for i in range(10000):
+for i in range(1000):
     random.shuffle(z)
     e=int(z[0]+z[1]+z[2]+z[3])
     x.append(e)
